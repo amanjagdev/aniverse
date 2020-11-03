67dcb075926105f57f01811cec837acaf4ddffa4
Brandon Leon <bsleon@umich.edu>
2020-11-03 00:52:14 -0500

    Add Enter key-press for search submit

--- src/HOC/Header/Header.js
+++ src/HOC/Header/Header.js
@@ -2,44 +2,58 @@ import React, { useState } from "react";
 import { withRouter, Link } from "react-router-dom";
 
 //Importing Styles
-import "./Header.css"
+import "./Header.css";
 
 const Header = (props) => {
-    const [searchText, setSearchText] = useState("");
-    const [searchType, setSearchType] = useState(0);
+	const [searchText, setSearchText] = useState("");
+	const [searchType, setSearchType] = useState(0);
 
-    const hanldeSearch = () => {
-        props.history.push({
-            pathname: '/search',
-            state: {
-                searchText: searchText,
-                searchType: searchType,
-            }
-        });
-    }
+    const handleSearch = (e) => {
+		e.preventDefault();
+		props.history.push({
+			pathname: "/search",
+			state: {
+				searchText: searchText,
+				searchType: searchType,
+			},
+		});
+	};
 
-    return (
-        <div className="Header">
-            <div className="Logo">
-                <Link to="/">
-                    <h4>Aniverse</h4>
-                </Link>
-            </div>
-            <div className="Search-Box">
-                <input
-                    type="text"
-                    value={searchText}
-                    onChange={(e) => setSearchText(e.target.value)}
-                    placeholder={"Search"}
-                />
-                <button onClick={() => hanldeSearch()}> <i className="fas fa-search"></i> </button>
-            </div>
-            <div className="Selector">
-                <button className={searchType === 0 ? "selected" : ""} onClick={() => setSearchType(0)}>Anime</button>
-                <button className={searchType === 1 ? "selected" : ""} onClick={() => setSearchType(1)}>Character</button>
-            </div>
-        </div>
-    );
+	return (
+		<div className="Header">
+			<div className="Logo">
+				<Link to="/">
+					<h4>Aniverse</h4>
+				</Link>
+			</div>
+			<form className="Search-Box" onSubmit={handleSearch}>
+				<input
+					type="text"
+					value={searchText}
+					onChange={(e) => setSearchText(e.target.value)}
+					placeholder={"Search"}
+				/>
+				<button type="submit">
+					{" "}
+					<i className="fas fa-search"></i>{" "}
+				</button>
+			</form>
+			<div className="Selector">
+				<button
+					className={searchType === 0 ? "selected" : ""}
+					onClick={() => setSearchType(0)}
+				>
+					Anime
+				</button>
+				<button
+					className={searchType === 1 ? "selected" : ""}
+					onClick={() => setSearchType(1)}
+				>
+					Character
+				</button>
+			</div>
+		</div>
+	);
 };
 
 export default withRouter(Header);
